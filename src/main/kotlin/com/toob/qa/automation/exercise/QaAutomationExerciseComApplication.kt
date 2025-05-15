3d0bcf2780243bdb6c6523455ae6a38c80cf1e4a
Thabo Matjuda <tl.matjuda@gmail.com>
2025-05-15 20:49:35 +0200

    Initial commit

---
+++ src/main/kotlin/com/toob/qa/automation/exercise/QaAutomationExerciseComApplication.kt
@@ -0,0 +1,11 @@
+package com.toob.qa.automation.exercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QaAutomationExerciseComApplication
+
+fun main(args: Array<String>) {
+	runApplication<QaAutomationExerciseComApplication>(*args)
+}
